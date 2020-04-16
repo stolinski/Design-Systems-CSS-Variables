dfc82b1da19015a65f6d3272eb4f30fc7a622ebd
Scott Tolinski <scott.tolinski@gmail.com>
2020-04-16 11:29:46 -0600

    initial commit

---
+++ theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    body: '"MonoLisa", system-ui, sans-serif',
+    monospace: '"MonoLisa", monospace',
+  },
+  colors: {
+    text: "#fff",
+    background: "#101010",
+    primary: "#e5bf62",
+  },
+  fontWeights: {
+    heading: 600,
+    bold: 400,
+  },
+  text: {
+    heading: {
+      color: "primary",
+      textAlign: "center",
+    },
+  },
+  styles: {
+    Slide: {
+      padding: "100px",
+    },
+    pre: {
+      borderRadius: "15px",
+    },
+    a: {
+      color: "white",
+    },
+    code: {
+      color: "primary",
+    },
+  },
+};
